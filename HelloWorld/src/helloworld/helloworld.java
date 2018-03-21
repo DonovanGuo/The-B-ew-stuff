7b955d2ec6bb05027d7e409b5e3d2c0254c5d06a
Steven <Steven@Steven>
2018-03-21 01:58:34 -0400

    Change it back if you want Was just for fun

--- HelloWorld/src/helloworld/helloworld.java
+++ HelloWorld/src/helloworld/helloworld.java
@@ -1,35 +1,63 @@
 package helloworld;
-	 
-	import javafx.application.Application;
-	import javafx.event.ActionEvent;
-	import javafx.event.EventHandler;
-	import javafx.scene.Scene;
-	import javafx.scene.control.Button;
-	import javafx.scene.layout.StackPane;
-	import javafx.stage.Stage;
-	 
-	public class helloworld extends Application {
-	    public static void main(String[] args) {
-	        launch(args);
-	    }
-	    
-	    @Override
-	    public void start(Stage primaryStage) {
-	        primaryStage.setTitle("Hello World!");
-	        Button btn = new Button();
-	        btn.setText("Say 'Hello World'");
-	        btn.setOnAction(new EventHandler<ActionEvent>() {
-	 
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Hello World!");
-	            }
-	        });
-	        
-	        StackPane root = new StackPane();
-	        root.getChildren().add(btn);
-	        primaryStage.setScene(new Scene(root, 300, 250));
-	        primaryStage.show();
-	    }
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.util.List;
+
+public class helloworld extends Application {
+	
+	private int i = 0;
+	private int clicks = 0;
+	public static void main(String[] args) {
+		launch(args);
 	}
+
+	@Override
+	public void start(Stage primaryStage) {
+
+		List<String> something = CSVReaderInJava.readFromCSV("idk.txt");
+		int elements = something.size();
+		
+		primaryStage.setTitle("NOT Hello World!");
+		Button btn = new Button();
+		Label label = new Label("Times clicked:" + clicks);
+		btn.setText(Integer.toString(i));
+		btn.setText("CLICK ME!!!!");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				if (i >= elements)
+				{
+					clicks++;
+					System.out.println("I Have Nothing More To Say");
+				}
+		
+				else if (i < elements)
+				{
+					clicks++;
+					System.out.println(something.get(i));
+					i++;
+				}
+				
+				label.setText("Times clicked:" + clicks);
+			
+			}
+		});
+
+		StackPane root = new StackPane();
+		root.setAlignment(label, Pos.TOP_CENTER);
+		root.getChildren().addAll(btn, label);
+		
+		primaryStage.setScene(new Scene(root, 300, 300));
+		primaryStage.show();
+	}
+}
 
